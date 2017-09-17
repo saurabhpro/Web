d88918a832a671ca8c6af54f20f6de675f61d922
Saurabh Kumar <saurabhk15@outlook.com>
2017-09-17 23:36:55 +0530

    ts vending machine with knockout and requirejs

---
+++ TypeScript/VendingMachine/js/productCategory.js
@@ -0,0 +1,61 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    class ProductCategory {
+        constructor() {
+            this.imgPath = "img/";
+        }
+    }
+    exports.ProductCategory = ProductCategory;
+    class SodaCategory extends ProductCategory {
+        constructor() {
+            super(...arguments);
+            this.name = "Soda";
+        }
+        getImageUrl() {
+            return this.imgPath + "SodaCan.png";
+        }
+    }
+    exports.SodaCategory = SodaCategory;
+    class NutsCategory extends ProductCategory {
+        constructor() {
+            super(...arguments);
+            this.name = "Nuts";
+        }
+        getImageUrl() {
+            return this.imgPath + "Nuts.png";
+        }
+    }
+    exports.NutsCategory = NutsCategory;
+    class ChipsCategory extends ProductCategory {
+        constructor() {
+            super(...arguments);
+            this.name = "Potato chips";
+        }
+        getImageUrl() {
+            return this.imgPath + "Chips.png";
+        }
+    }
+    exports.PotatoChipsCategory = ChipsCategory;
+    class CandyCategory extends ProductCategory {
+        constructor() {
+            super(...arguments);
+            this.name = "Candy";
+        }
+        getImageUrl() {
+            return this.imgPath + "Candy.png";
+        }
+    }
+    exports.CandyCategory = CandyCategory;
+    class CandyBarCategory extends ProductCategory {
+        constructor() {
+            super(...arguments);
+            this.name = "Candy bar";
+        }
+        getImageUrl() {
+            return this.imgPath + "CandyBar.png";
+        }
+    }
+    exports.CandyBarCategory = CandyBarCategory;
+});
+//# sourceMappingURL=productCategory.js.map
