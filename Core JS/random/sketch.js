d29a8d8b04fc7714fd9b763161b1ffc70941a62d
Saurabh Kumar <saurabhk15@outlook.com>
2017-11-11 22:48:00 +0530

    making function in argument

--- Core JS/random/sketch.js
+++ Core JS/random/sketch.js
@@ -2,10 +2,15 @@ function setup() {
   createCanvas(640, 360);
   background(0);
   let button = createButton("press");
-  button.mousePressed(changeBackground);
+  //  button.mousePressed(changeBackground);
+  
+  button.mousePressed(function changeBackground() {
+    background(random(255));
+  });
+
 }
 
 
-function changeBackground() {
-  background(random(255));
-}
+// function changeBackground() {
+//   background(random(255));
+// }
