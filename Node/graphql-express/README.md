e8165ffebafd730571e36f16ee1f554849dcc421
Saurabh Kumar <saurabh.kumar@cvent.com>
2021-03-31 10:00:24 +0530

    basic query on books types

--- Node/graphql-express/README.md
+++ Node/graphql-express/README.md
@@ -4,7 +4,7 @@
 - https://www.youtube.com/watch?v=ZQL7tL2S0oQ
 - https://github.com/WebDevSimplified/Learn-GraphQL/blob/master/server.js
 
-## Level 1
+## Level 1 - GraphQL Hello World
 ```js
 // input
 query FirstMessage {
@@ -15,6 +15,58 @@ query FirstMessage {
 {
   "data": {
     "message": "Hello World"
+  }
+}
+```
+
+## Level 2 - GraphQL Return Books (note: we are not displaying the books author name)
+```js
+// input
+{
+  books {
+    name
+    authorId
+  }
+}
+
+
+// output
+{
+  "data": {
+    "books": [
+      {
+        "name": "Harry Potter and the Chamber of Secrets",
+        "authorId": 1
+      },
+      {
+        "name": "Harry Potter and the Prisoner of Azkaban",
+        "authorId": 1
+      },
+      {
+        "name": "Harry Potter and the Goblet of Fire",
+        "authorId": 1
+      },
+      {
+        "name": "The Fellowship of the Ring",
+        "authorId": 2
+      },
+      {
+        "name": "The Two Towers",
+        "authorId": 2
+      },
+      {
+        "name": "The Return of the King",
+        "authorId": 2
+      },
+      {
+        "name": "The Way of Shadows",
+        "authorId": 3
+      },
+      {
+        "name": "Beyond the Shadows",
+        "authorId": 3
+      }
+    ]
   }
 }
 ```
--- Node/graphql-express/server.js
+++ Node/graphql-express/server.js
@@ -1,20 +1,52 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { books } from './datastore.js';
 
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
+import {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull,
+} from 'graphql';
 
 const app = express();
 
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'FirstMessage',
-        fields: () => ({
-            message: {
-                type: GraphQLString,
-                resolve: () => 'Hello World',
-            },
-        }),
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    description: 'This represents a book written by an author',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt) },
+        name: { type: GraphQLNonNull(GraphQLString) }, // means we decorate the String type as non-null
+        authorId: { type: GraphQLNonNull(GraphQLInt) },
     }),
+});
+
+// we can create a RootQueryType to store all our typs and query feilds
+const RootQueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'Root Query',
+    fields: () => ({
+        book: {
+            type: BookType,
+            description: 'A Single Book',
+            args: {
+                id: { type: GraphQLInt },
+            },
+            resolve: (parent, args) =>
+                books.find((book) => book.id === args.id),
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            description: 'List of All Books',
+            resolve: () => books,
+        },
+    }),
+});
+
+const schema = new GraphQLSchema({
+    query: RootQueryType,
 });
 
 app.use(
