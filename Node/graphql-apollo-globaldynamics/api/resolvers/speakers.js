89418d5cc171d0837cf785ec4dd042cefc8ce0be
Saurabh Kumar <saurabh.kumar@cvent.com>
2021-06-11 10:36:27 +0530

    fix speakers and sessions

--- Node/graphql-apollo-globaldynamics/api/resolvers/speakers.js
+++ Node/graphql-apollo-globaldynamics/api/resolvers/speakers.js
@@ -1,9 +1,12 @@
-const sessions = async (speaker, args, { dataSources }) => {
-  const sessionsList = await dataSources.sessionDataSource.getSessions();
+import _ from 'lodash';
 
-  return sessionsList.filter(
-    (session) => filter(speaker.sessions, { id: session.id }).length > 0
-  );
+export default {
+  sessions: async (speaker, args, { dataSources }) => {
+    console.log(speaker);
+    const sessionsList = await dataSources.sessionDataSource.getSessions();
+
+    return sessionsList.filter(
+      (session) => _.filter(speaker.sessions, { id: session.id }).length > 0
+    );
+  },
 };
-
-export default sessions;
