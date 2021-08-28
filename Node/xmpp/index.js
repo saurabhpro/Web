a83695c7c54b4e57a546c466bf0442c9adebe018
Saurabh Kumar <saurabhk1511@gmail.com>
2021-08-28 18:56:37 +0530

    ex6 update to xmpp

--- Node/xmpp/index.js
+++ Node/xmpp/index.js
@@ -1,27 +1,28 @@
-const moment = require('moment');
-const xmpp = require('simple-xmpp');
+import moment from 'moment';
+import pkg from 'simple-xmpp';
+const { send, on, connect } = pkg;
 
 const sendMessage = () => {
   setTimeout(sendMessage, 3000);
-  xmpp.send(
+  send(
     'saurabh@localhost',
     `hi! ${moment().format('MMMM Do YYYY, h:mm:ss a')}`
   );
 };
 
-xmpp.on('online', (data) => {
+on('online', (data) => {
   console.log('Hay you are online');
   console.log(`connected as ${data.jid.user}`);
   sendMessage();
 });
 
-xmpp.on('error', (error) => console.log(`something went wrong! ${error}`));
+on('error', (error) => console.log(`something went wrong! ${error}`));
 
-xmpp.on('chat', (from, message) => {
+on('chat', (from, message) => {
   console.log(`Got a message~ ${message} from ${from}`);
 });
 
-xmpp.connect({
+connect({
   jid: 'admin@localhost',
   password: 'password',
   host: 'localhost',
--- Node/xmpp/saurabh.js
+++ Node/xmpp/saurabh.js
@@ -1,8 +1,9 @@
-const moment = require('moment');
-const xmpp = require('simple-xmpp');
+import moment from 'moment';
+import pkg from 'simple-xmpp';
+const { send, on, connect } = pkg;
 
 //saurabh
-xmpp.on('online', (data) => {
+on('online', (data) => {
   console.log('Hey you are online! ');
   console.log(`Connected as ${data.jid.user}`);
   sendMessage();
@@ -13,19 +14,16 @@ xmpp.on('online', (data) => {
  */
 const sendMessage = () => {
   setTimeout(sendMessage, 5000);
-  xmpp.send(
-    'admin@localhost',
-    `hi! ${moment().format('MMMM Do YYYY, h:mm:ss a')}`
-  );
+  send('admin@localhost', `hi! ${moment().format('MMMM Do YYYY, h:mm:ss a')}`);
 };
 
-xmpp.on('error', (error) => console.log(`something went wrong!${error} `));
+on('error', (error) => console.log(`something went wrong!${error} `));
 
-xmpp.on('chat', (from, message) => {
+on('chat', (from, message) => {
   console.log(`Got a message! ${message} from ${from}`);
 });
 
-xmpp.connect({
+connect({
   jid: 'saurabh@localhost',
   password: 'password',
   host: 'localhost',
